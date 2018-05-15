016cdef12f39d19b5bd478e8e1986fd4e4942798
Can ÇALIŞKAN <cancaliskan@windowslive.com>
2018-05-15 13:40:48 +0300

    bildirim eklendi ve arama sorunu giderildi

---
+++ WebSite/araniyor_15052018/araniyor/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace araniyor.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<araniyor.Models.Araniyor>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "araniyor.Models.Araniyor";
+        }
+
+        protected override void Seed(araniyor.Models.Araniyor context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
